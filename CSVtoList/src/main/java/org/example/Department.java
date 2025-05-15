bab2a316bdbf588cb06e161cbf4378e936a79675
BrightSparkle <ekaterinamrch@gmail.com>
2025-05-15 12:24:01 +0300

    Create class Department

---
+++ CSVtoList/src/main/java/org/example/Department.java
@@ -0,0 +1,34 @@
+package org.example;
+import java.util.HashMap;
+import java.util.Map;
+
+public class Department {
+    private static final Map<String, Department> departments = new HashMap<>();
+    private static int nextId = 1;
+
+    private final int id;
+    private final String name;
+
+    private Department(String name) {
+        this.id = nextId++;
+        this.name = name;
+    }
+
+    public static Department getInstance(String name) {
+        departments.putIfAbsent(name, new Department(name));
+        return departments.get(name);
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    @Override
+    public String toString() {
+        return "Department{id=" + id + ", name='" + name + "'}";
+    }
+}
